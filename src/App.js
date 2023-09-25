b45d999f53fc7661af0da644ea724035d8ee2e5f
melyu123 <126828260+melyu123@users.noreply.github.com>
2023-09-25 13:35:15 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,125 +9,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
 
 
-
-const [todoList, setTodoList] = useState([]);
-
-const [isLoading, setIsLoading] = useState(true);
-
-const [editing, setEditing] = useState(false);
-
-const [todoTitle,setTodoTitle]= useState('');
-const [editID, setEditID] = useState(null);
-
- const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}/`;
- console.log(url)
- const fetchData = async() =>{
-   
-    const options = {method:'GET', headers:{'Authorization':`Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`}};
-    console.log(options)
-     
-     try {
-      const response = await
-         fetch(url, options);
-         if (!response.ok) {
-            const message = `Error: ${response.status}`;
-            throw new Error(message);
-           }
-
-           const data = await response.json();
-           
-           const todos = data.records.map((todo)=>{
-           const newTodo = {
-              id: todo.id,
-              title:todo.fields.title,
-              todo:todo.fields.todo
-            }
-
-          return newTodo || [];
-
-           });
-
-
-           setTodoList(todos);
-           setIsLoading(false);
-           
-          } catch (error) {
-              console.log(error.message);
-          
-    }
-    
-}
-
-  useEffect(()=>{
-
-    fetchData()},[]);
-
-
- 
-  useEffect(() => {
-    if (!isLoading) {
-      localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-
-    };
-     },[todoList,isLoading]);
-
-
- function removeTodo(id) {
-      setTodoList(todoList.filter((todo) => todo.id !== id));
-     }
-
-  
-
-  const addTodo =(newTodo)=>{
-    setTodoList([...todoList,newTodo]);
-
-  }
-
-
-  const clearall =()=>{
-    setTodoList('');
-  }
-
-  const edit=(id)=>{
-    
-       const selectItem = todoList.find(item =>item.id ===id);
-       setEditing(true);
-       setEditID(id)
-       setTodoTitle(selectItem.title)
- }
-
-  const handleTitleChange  =(event) =>{
-
-      const newTodoTitle = event.target.value;
-       setTodoTitle(newTodoTitle);
-  }
-
- 
- const handleAddTodo = (event) =>{
-      event.preventDefault();
-
-      if(!todoTitle){
-
-      }else if(todoTitle && editing){
-        console.log(todoList)
-        setTodoList(
-        todoList.map(item => {
-            
-        if(item.id === editID){
-          return {...item, title:todoTitle}
-         }
-          return item
-import React, { useState, useEffect } from 'react';
-import styles from './App.module.css'
-import AddTodoForm from './AddTodoForm';
-import TodoList from './TodoList';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-
-
-function App() {
-
-
 const [todoList, setTodoList] = useState([]);
 
 const [isLoading, setIsLoading] = useState(true);
@@ -239,7 +120,7 @@ const [editID, setEditID] = useState(null);
         
         addTodo( {title:todoTitle, id:Date.now()});
         setTodoTitle('');
-git 
+
       }
  }
      
